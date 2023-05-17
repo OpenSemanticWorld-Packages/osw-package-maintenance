cc3ed8e12b5e44e814d2c9ea3f903ff8b7bf3f2f
Lukas Gold <lukas.gold@isc.fraunhofer.de>
2023-05-17 12:19:57 +0200

    Cosmetic refactoring and docstrings

--- scripts/reusable.py
+++ scripts/reusable.py
@@ -6,6 +6,8 @@ from pydantic import FilePath
 
 
 class WorldMeta(Package):
+    """Metadata for the world.opensemantic page packages"""
+
     # name: is required but not provided here -> needs to be set in each script
     # repo: is required but not provided here -> needs to be set in each script
     # id: is required but not provided here -> needs to be set in each script
@@ -21,6 +23,8 @@ class WorldMeta(Package):
 
 
 class WorldCreat(Package.CreationConfig):
+    """Creation config for the world.opensemantic page packages"""
+
     domain = "wiki-dev.open-semantic-lab.org"
     credentials_file_path: Optional[FilePath] = (
         Path(__file__).parent / "accounts.pwd.yaml"
@@ -29,12 +33,16 @@ class WorldCreat(Package.CreationConfig):
 
 
 class OslMeta(Package):
+    """Metadata for the org.open-semantic-lab page packages"""
+
     repo_org = "OpenSemanticLab"
     language = "en"
     publisher = "OpenSemanticLab"
 
 
 class OslCreat(Package.CreationConfig):
+    """Creation config for the org.open-semantic-lab page packages"""
+
     domain = "wiki-dev.open-semantic-lab.org"
     credentials_file_path: Optional[FilePath] = (
         Path(__file__).parent / "accounts.pwd.yaml"
