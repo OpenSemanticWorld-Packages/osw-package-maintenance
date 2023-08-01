1572cc55fd6ef0c8596b66d2e710c666f72665a8
Lukas Gold <lukas.gold@isc.fraunhofer.de>
2023-08-01 17:22:43 +0200

    Extension of world.opensemantic.base

--- scripts/world.opensemantic.base.py
+++ scripts/world.opensemantic.base.py
@@ -17,7 +17,7 @@ package_meta_data = WorldMeta(
     # Provide a package description
     description=("Provides base items like Article, Person, Project"),
     # Specify the package version - use semantic versioning
-    version="0.11.0",
+    version="0.12.0",
     # Author(s)
     author=["Simon Stier", "Lukas Gold", "Alexander Triol"],
     # List the full page titles of the pages to be included in the package
@@ -28,7 +28,7 @@ package_meta_data = WorldMeta(
         "Category:OSW0e084decca6f48a7b023d6b7b2c1452d",  # Event
         "Category:OSW81e9e22e7d934382a6a56df7d3736957",  # Recipe
         "Category:OSWa5812d3b5119416c8da1606cbe7054eb",  # Term
-        "Category:OSWfe72974590fd4e8ba94cd4e8366375e8", # DataSet
+        "Category:OSWfe72974590fd4e8ba94cd4e8366375e8",  # DataSet
         "Category:OSW3d238d05316e45a4ac95a11d7b24e36b",  # Location
         "Category:OSWf0fe562f422d49c6877490b3dfee2f3f",  # Device
         "Category:OSW02590972aeba46d7864ed492c0c11384",  # Host
@@ -40,9 +40,26 @@ package_meta_data = WorldMeta(
         "Category:OSWd9aa0bca9b0040d8af6f5c091bf9eec7",  # User
         "Category:OSWb2d7e6a2eff94c82b7f1f2699d5b0ee3",  # Project
         "Category:OSWd22af0aa3b00462a9da9b509538e8926",  # FundingCall
-        "Item:OSWf474ec34b7df451ea8356134241aef8a", # State:Done
-        "Item:OSWa2b4567ad4874ea1b9adfed19a3d06d1", # State:In work
-        "Item:OSWaa8d29404288446a9f3ec7afa4e2a512", # State:To do
+        "Item:OSWf474ec34b7df451ea8356134241aef8a",  # State:Done
+        "Item:OSWa2b4567ad4874ea1b9adfed19a3d06d1",  # State:In work
+        "Item:OSWaa8d29404288446a9f3ec7afa4e2a512",  # State:To do
+        "Category:OSW4bcd4a99a73f482ea40ac4210dfab836",  # Building
+        "Category:OSW6c4212f1a39342be963d2b9efd19c5c2",  # Floor
+        "Category:OSWc5ed0ed1e33c4b31887c67af25a610c1",  # Room
+        "Category:OSWac9f0e49d8024804bd7d77058322a3fe",  # RoomUsage
+        "Category:OSWae92be81cdb34d22844d4791ef790d93",  # AreaUsageType
+        "Category:OSW1969007d5acf40539642877659a02c23",  # Organization
+        "Category:OSWd9521d3054814dd29c2bcdbd9185d1f0",  # Association
+        "Category:OSW5f4a3751d23e482d80fb0b72dcd6bc31",  # Corporation
+        "Category:OSWd7085ef89b0e4a69ac4f2d28bda2d2c0",  # Foundation
+        "Category:OSW11ee14fb9f774b4b89bdb9bb89aac14d",  # University
+        "Category:OSWfe3e842b804445c7bb0dd8ee61da2d70",  # OrganizationalSubUnit
+        "Category:OSW94aa074255374580b70337340c5ccc1b",  # Department
+        "Category:OSWa01126bc9e9048988cb0f49e359015bc",  # Faculty
+        "Category:OSWb8b6278763d54b0784eea9d3b3d183a4",  # Group
+        "Category:OSW5427361692374c8eaa6bd3733b92d343",  # Institute
+        "Category:OSW5efde23b1d8c4e1c864ef039cb0616ed",  # PersonRole
+        "Category:OSW07a0faef5be94b788514a2dd5dca20bf",  # BuildingFunction
     ],
 )
 # Provide the information needed (only) to create the page package
