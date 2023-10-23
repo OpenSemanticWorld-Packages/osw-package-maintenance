c087a629208a0be7f689ffec3ed94f437d5031ec
SimonTaurus <simon.stier@gmx.de>
2023-10-23 03:53:54 +0200

    fix: add missing DataTool

--- scripts/world.opensemantic.lab.py
+++ scripts/world.opensemantic.lab.py
@@ -17,7 +17,7 @@ package_meta_data = WorldMeta(
     # Provide a package description
     description=("For material science, etc."),
     # Specify the package version - use semantic versioning
-    version="0.3.0",
+    version="0.3.1",
     # Author(s)
     author=["Simon Stier"],
     # List the full page titles of the pages to be included in the package
@@ -29,6 +29,7 @@ package_meta_data = WorldMeta(
         "Category:OSW88894b63a51d46b08b5b4b05a6b1b3c3",  # Sample
         "Category:OSW29f0a4619cc243679e68b682d3bdb890",  # GeoSample
         "Category:OSW0e7fab2262fb4427ad0fa454bc868a0d",  # ElnEntry
+        "Category:OSWda27e2fff10848ebb728ffb69c49a16d",  # DataTool"
         "Property:HasCount",
         "Property:HasMass",
         "Property:HasVolume",
