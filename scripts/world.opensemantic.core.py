78ed60344c7f2337605e9d692e24ad021f21f617
SimonTaurus <simon.stier@gmx.de>
2023-11-14 07:38:49 +0100

    update core package

--- scripts/world.opensemantic.core.py
+++ scripts/world.opensemantic.core.py
@@ -19,7 +19,7 @@ package_meta_data = WorldMeta(
         "Provides core functionalities of OpenSemanticWorld / OpenSemanticLab"
     ),
     # Specify the package version - use semantic versioning
-    version="0.21.0",
+    version="0.24.0",
     # Specify the required MediaWiki extensions
     requiredExtensions=[
         "OpenSemanticLab",
@@ -49,14 +49,18 @@ package_meta_data = WorldMeta(
         "Category:OSW2ac4493f8635481eaf1db961b63c8325",  # Data
         "Category:OSWff333fd349af4f65a69100405a9e60c7",  # File
         "Category:OSW3e3f5dd4f71842fbb8f270e511af8031", # LocalFile
-        "Category:OSW05b244d0a669436e96fe4e1631d5a171", # RemmoteFile
+        "Category:OSW05b244d0a669436e96fe4e1631d5a171", # RemoteFile
         "Category:OSW11a53cdfbdc24524bf8ac435cbf65d9d",  # WikiFile
         "Category:OSWe5aa96bffb1c4d95be7fbd46142ad203",  # Process
-        "Category:OSW9725d7a91bab4f1aa68f423e4e9bfcf4", # State
         "Category:OSWc5d4829ed2744a219ba027171c75fa1d", # Task
+        "Category:OSW9725d7a91bab4f1aa68f423e4e9bfcf4", # State
         "Item:OSWf474ec34b7df451ea8356134241aef8a",  # State:Done
         "Item:OSWa2b4567ad4874ea1b9adfed19a3d06d1",  # State:In work
         "Item:OSWaa8d29404288446a9f3ec7afa4e2a512",  # State:To do
+        "Category:OSW65c8449bdd4f4fbcb7f68203a11d6e8f", # Priority
+        "Item:OSW8743c7d03c4e46c1bd42bb05e1a082d9", # High
+        "Item:OSW8d781c35212548fa9b2fccad3765da65", # Medium
+        "Item:OSWcaf7db070ad6407babc5245e84d76840", # Low
         "Category:OSWe427aafafbac4262955b9f690a83405d",  # Tool
         "Category:Property",
         "Category:AnnotationProperty",
