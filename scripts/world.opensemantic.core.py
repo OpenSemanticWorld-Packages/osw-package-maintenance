d2f3d51e99b9a0da47798ac4d9f2cd889d70e7b2
Lukas Gold <lukas.gold@isc.fraunhofer.de>
2024-02-06 17:30:05 +0100

    rdf_type moved + utf-8 encoded serialization

--- scripts/world.opensemantic.core.py
+++ scripts/world.opensemantic.core.py
@@ -19,7 +19,7 @@ package_meta_data = WorldMeta(
         "Provides core functionalities of OpenSemanticWorld / OpenSemanticLab"
     ),
     # Specify the package version - use semantic versioning
-    version="0.30.0",
+    version="0.30.1",
     # Specify the required MediaWiki extensions
     requiredExtensions=["OpenSemanticLab", "ExternalData", "WikiMarkdown"],
     # Specify the required PagePackages
