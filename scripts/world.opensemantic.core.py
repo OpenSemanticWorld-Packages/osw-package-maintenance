2e431394e4a019a4e270c7975ad7417c3ad81f25
Lukas Gold <lukas.gold@isc.fraunhofer.de>
2024-02-08 16:07:59 +0100

    QR Code section added to Category:Item footer_template

--- scripts/world.opensemantic.core.py
+++ scripts/world.opensemantic.core.py
@@ -19,7 +19,7 @@ package_meta_data = WorldMeta(
         "Provides core functionalities of OpenSemanticWorld / OpenSemanticLab"
     ),
     # Specify the package version - use semantic versioning
-    version="0.30.2",
+    version="0.30.3",
     # Specify the required MediaWiki extensions
     requiredExtensions=["OpenSemanticLab", "ExternalData", "WikiMarkdown"],
     # Specify the required PagePackages
