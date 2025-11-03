4cdfde1e7e36a93d621d1ee7e83711f1aecd3771
Lukas Gold <lukas.gold@isc.fraunhofer.de>
2025-11-03 17:19:32 +0100

    Added some essentials for handling of chemical substances and testing

--- scripts/world.opensemantic.lab.py
+++ scripts/world.opensemantic.lab.py
@@ -17,7 +17,7 @@ package_meta_data = WorldMeta(
     # Provide a package description
     description=("For material science, etc."),
     # Specify the package version - use semantic versioning
-    version="0.5.0",
+    version="0.6.0",
     # Specify the required PagePackages
     requiredPackages=[
         "world.opensemantic.base",
