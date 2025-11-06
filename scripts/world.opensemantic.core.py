fdd2166bad4c3dac51b18196d0b307a79f944f5f
SimonTaurus <simon.stier@gmx.de>
2025-11-06 03:51:30 +0100

    update core

--- scripts/world.opensemantic.core.py
+++ scripts/world.opensemantic.core.py
@@ -19,7 +19,7 @@ package_meta_data = WorldMeta(
         "Provides core functionalities of OpenSemanticWorld / OpenSemanticLab"
     ),
     # Specify the package version - use semantic versioning
-    version="0.49.5",
+    version="0.49.6",
     # Specify the required MediaWiki extensions
     requiredExtensions=["OpenSemanticLab", "ExternalData", "WikiMarkdown"],
     # Specify the required PagePackages
