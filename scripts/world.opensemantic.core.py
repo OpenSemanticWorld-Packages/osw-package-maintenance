73da88f0dbc9b0f961facfcd1dce59f0580b0db2
Lukas Gold <lukas.gold@isc.fraunhofer.de>
2025-11-14 17:11:23 +0100

    Fix: removed anchor uuid in template & some minor things
    - DocumentationExtension
    - typo in ProcessType
    - some date-time
    - missing title UUID added
    - missing reference to content in packages.json

--- scripts/world.opensemantic.core.py
+++ scripts/world.opensemantic.core.py
@@ -19,7 +19,7 @@ package_meta_data = WorldMeta(
         "Provides core functionalities of OpenSemanticWorld / OpenSemanticLab"
     ),
     # Specify the package version - use semantic versioning
-    version="0.49.6",
+    version="0.50.0",
     # Specify the required MediaWiki extensions
     requiredExtensions=["OpenSemanticLab", "ExternalData", "WikiMarkdown"],
     # Specify the required PagePackages
