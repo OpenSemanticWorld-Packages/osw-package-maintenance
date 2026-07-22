075a71cd9e42cedd07401b2a705c67f2ee48e965
SimonTaurus <simon.stier@gmx.de>
2026-07-22 07:45:56 +0200

    update batteries and core

--- scripts/world.opensemantic.batteries.py
+++ scripts/world.opensemantic.batteries.py
@@ -19,7 +19,7 @@ package_meta_data = WorldMeta(
         "Provides battery specific items like ElectrochemicalTest, BatteryCell, etc."
     ),
     # Specify the package version - use semantic versioning
-    version="1.2.0",
+    version="1.2.2",
     # Specify the required PagePackages
     requiredPackages=["world.opensemantic.base", "world.opensemantic.lab"],
     # Author(s)
@@ -143,7 +143,7 @@ package_meta_data = WorldMeta(
         "Category:OSW50c8314e8b2c416db73ce935f1dd8d22",  # Chemical system
         "Category:OSWb3b9ec5cb50d401c9b996ce3009b5c54",  # Environmental test chamber
         "Category:OSW83c7be482a5e4e349f9875194e6f180d",  # Form factor
-        "Category:OSWa211cf52e86e47929768a769235f7144",  # Format
+        "Category:OSWa211cf52e86e47929768a769235f7144",  # BatteryFormat
         "Property:HasFormat",
         "Property:HasFormFactor",
 
